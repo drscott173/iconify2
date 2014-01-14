b7ed498d08b3846f459edd52681e167d934b1e3f
Scott Penberthy <scott.penberthy@gmail.com>
2014-01-13 19:46:02 -0500

    Initial Commit

---
+++ iconify/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  iconify
+//
+//  Created by Scott Penberthy on 1/13/14.
+//  Copyright (c) 2014 Scott Penberthy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
